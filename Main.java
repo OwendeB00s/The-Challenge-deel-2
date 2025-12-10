d14b93360c2218353ad3d8c10a33a0a3b7258da8
fahdbdg <25099558@student.hhs.nl>
2025-12-10 13:42:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -19,7 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         root = new BorderPane();
 
-
+       
         homeBtn = new Button("Home");
         historyBtn = new Button("Geschiedenis");
         settingsBtn = new Button("Instellingen");
@@ -28,30 +28,16 @@ public class Main extends Application {
 
         updateView("Welkom!", homeBtn);
 
-
+        
         navBar = new HBox(10, historyBtn, homeBtn, settingsBtn);
         navBar.setAlignment(Pos.CENTER);
+        navBar.setPadding(new Insets(10));
         root.setBottom(navBar);
 
 
         homeBtn.setOnAction(e -> updateView("Welkom!", homeBtn));
         historyBtn.setOnAction(e -> updateView("Geschiedenis (inhoud komt nog)", historyBtn));
-
-        settingsBtn.setOnAction(e -> {
-
-            VBox settingsView = new VBox(20);
-            settingsView.setAlignment(Pos.CENTER);
-            Text settingsText = new Text("Instellingen (inhoud komt nog)");
-            settingsView.getChildren().addAll(settingsText, accountBtn);
-            root.setCenter(settingsView);
-
-
-            homeBtn.setDisable(false);
-            historyBtn.setDisable(false);
-            settingsBtn.setDisable(true);
-            accountBtn.setDisable(false);
-        });
-
+        settingsBtn.setOnAction(e -> openSettings());
         accountBtn.setOnAction(e -> updateView("Account", accountBtn));
 
 
@@ -74,5 +60,66 @@ public class Main extends Application {
         if (activeBtn != null) {
             activeBtn.setDisable(true);
         }
+    }
+
+    private void openSettings() {
+        VBox settingsBox = new VBox(20);
+        settingsBox.setPadding(new Insets(20));
+        settingsBox.setAlignment(Pos.CENTER);
+
+        Button personalDataBtn = new Button("Personal Data");
+        Button connectDeviceBtn = new Button("Connect Device");
+        Button logoutBtn = new Button("Logout");
+
+
+        personalDataBtn.setMinWidth(200);
+        connectDeviceBtn.setMinWidth(200);
+        logoutBtn.setMinWidth(200);
+        accountBtn.setMinWidth(200);
+
+        settingsBox.getChildren().addAll(
+                personalDataBtn,
+                connectDeviceBtn,
+                logoutBtn,
+                accountBtn
+        );
+
+        root.setCenter(settingsBox);
+
+       
+        homeBtn.setDisable(false);
+        historyBtn.setDisable(false);
+        settingsBtn.setDisable(true);
+        accountBtn.setDisable(false);
+
+
+        personalDataBtn.setOnAction(ev ->
+                updateView("No data found", personalDataBtn)
+        );
+
+        connectDeviceBtn.setOnAction(ev -> {
+            TextInputDialog codeDialog = new TextInputDialog();
+            codeDialog.setTitle("Connect Device");
+            codeDialog.setHeaderText("Voer de verbindingscode in:");
+            codeDialog.setContentText("Code:");
+            codeDialog.showAndWait();
+        });
+
+        logoutBtn.setOnAction(ev -> {
+            Alert logoutAlert = new Alert(Alert.AlertType.CONFIRMATION);
+            logoutAlert.setTitle("Logout");
+            logoutAlert.setHeaderText("Are you sure you want to log out?");
+            logoutAlert.setContentText("Klik Ja of Nee");
+
+            ButtonType yesBtn = new ButtonType("Ja");
+            ButtonType noBtn = new ButtonType("Nee");
+
+            logoutAlert.getButtonTypes().setAll(yesBtn, noBtn);
+            logoutAlert.showAndWait();
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
